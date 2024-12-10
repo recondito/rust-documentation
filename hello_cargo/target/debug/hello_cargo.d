3e7b38b31a203184295aafb30c88ee0212b68676
recondito <santanallanes@hotmail.com>
2024-12-09 22:28:06 -0400

    Hello Cargo

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/lileh/Rust/rust-documentation/hello_cargo/target/debug/hello_cargo: /home/lileh/Rust/rust-documentation/hello_cargo/src/main.rs
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/lileh/Rust/rust-documentation/hello_cargo/target/release/hello_cargo: /home/lileh/Rust/rust-documentation/hello_cargo/src/main.rs
