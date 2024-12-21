fdb282317f59fb455559742d7bc0b845d82578e1
recondito <santanallanes@hotmail.com>
2024-12-21 12:16:29 -0400

    Finished variables section.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -20,23 +20,35 @@ fn main() {
     // println!("The value of x is: {x}");
 
     // Invalid Array Element Access
-    let a = [1, 2, 3, 4, 5];
+    // let a = [1, 2, 3, 4, 5];
 
-    println!("Please enter an array index.");
+    // println!("Please enter an array index.");
 
-    let mut index = String::new();
+    // let mut index = String::new();
 
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
+    // io::stdin()
+    //     .read_line(&mut index)
+    //     .expect("Failed to read line");
 
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index entered was not a number");
+    // let index: usize = index
+    //     .trim()
+    //     .parse()
+    //     .expect("Index entered was not a number");
 
-    let element = a[index];
+    // let element = a[index];
 
-    println!("The value of the element at index {index} is: {element}");
+    // println!("The value of the element at index {index} is: {element}");
 
+    // Functions
+    println!("Hello, world!");
+    another_function(5);
+    print_labeled_measurement(5, 'h');
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
